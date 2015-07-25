cd6de745719d569870d1e69d5cb9d7be576b43cd
Gabe Stein <gabe@ghscommunications.com>
2015-07-25 13:41:31 -0600

    changes app to root

--- entry.js
+++ entry.js
@@ -1,16 +1,16 @@
 var React = require('react/addons');
-var App = require('./jsx/App.jsx');
+var Root = require('./jsx/Root.jsx');
 
 // exports less to dist/style.css per extract-text-webpack-plugin
 require('./less/app.less');
 
 // export required by static gen webpack plugin. Runs on server, returns html via callback
 module.exports = function(path, props, callback) {
-    var html = '<!DOCTYPE html>' + React.renderToString(React.createElement(App, {}));
+    var html = '<!DOCTYPE html>' + React.renderToString(React.createElement(Root, {}));
     callback(html);
 }
 
 // checks to see if we're in browser context, and renders
 if(typeof document !== 'undefined') {
-    React.render(React.createElement(App, {}), document);
+    React.render(React.createElement(Root, {}), document);
 }
