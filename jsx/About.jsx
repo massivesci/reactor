936d56039836f619ddae43e4ca8fe4ce035b5970
Gabe Stein <gabe@ghscommunications.com>
2015-07-25 13:57:46 -0600

    adds react router

---
+++ jsx/About.jsx
@@ -0,0 +1,19 @@
+/**********************************************************\
+** About.jsx                                              **
+**                                                        **
+** The root node of the Reactor app.                      **
+**                                                        **
+** Designed and engineered by Worb Corp.                  **
+\**********************************************************/
+
+var React = require('react');
+
+var About = React.createClass({
+  render: function(){
+    return (
+      <div>About</div>
+    );
+  }
+});
+
+module.exports = About;
---
+++ jsx/Home.jsx
@@ -0,0 +1,19 @@
+/**********************************************************\
+** Home.jsx                                               **
+**                                                        **
+** The root node of the Reactor app.                      **
+**                                                        **
+** Designed and engineered by Worb Corp.                  **
+\**********************************************************/
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function(){
+    return (
+      <div>Home</div>
+    );
+  }
+});
+
+module.exports = Home;
